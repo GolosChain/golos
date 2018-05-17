b6e82ad592f3705d98cd39e349c8ebc01cf238b3
Maslenitsa93 <maslenitsa93@gmail.com>
2018-05-17 15:31:07 +0300

    Mongo_db

---
+++ plugins/mongo_db/mongo_db_deleter.cpp
@@ -0,0 +1,91 @@
+#include <golos/plugins/mongo_db/mongo_db_deleter.hpp>
+#include <golos/plugins/mongo_db/mongo_db_writer.hpp>
+
+#include <fc/log/logger.hpp>
+
+#include <mongocxx/exception/exception.hpp>
+
+
+namespace golos {
+namespace plugins {
+namespace mongo_db {
+
+    using bsoncxx::builder::stream::finalize;
+
+    mongo_db_deleter::mongo_db_deleter() : mongo_db_connector() {
+    }
+
+    mongo_db_deleter::~mongo_db_deleter() {
+    }
+
+    bool mongo_db_deleter::remove_document(const named_document_ptr& named_doc) {
+        if (removed_blocks.find(named_doc->collection_name) == removed_blocks.end()) {
+            removed_blocks[named_doc->collection_name] = std::make_unique<mongocxx::bulk_write>(bulk_opts);
+        }
+
+        auto view = named_doc->doc.view();
+        auto itr = view.find("_id");
+        if (view.end() == itr) {
+            return false;
+        }
+        document filter;
+        filter << "_id" << view["_id"].get_oid();
+        mongocxx::model::delete_one msg{filter.view()};
+        removed_blocks[named_doc->collection_name]->append(msg);
+
+        return true;
+    }
+
+    void mongo_db_deleter::write_changes() {
+        auto iter = removed_blocks.begin();
+        for (; iter != removed_blocks.end(); ++iter) {
+
+            auto& oper = *iter;
+            try {
+                const std::string& collection_name = oper.first;
+                mongocxx::collection _collection = mongo_database[collection_name];
+
+                auto& bulkp = oper.second;
+                if (!_collection.bulk_write(*bulkp)) {
+                    ilog("Failed to remove documents from Mongo DB");
+                }
+            }
+            catch (const std::exception& e) {
+                ilog("Unknown exception while remove documents from mongo: ${e}", ("e", e.what()));
+                // If we got some errors removing document from mongo just skip this document and move on
+                removed_blocks.erase(iter);
+                throw;
+            }
+        }
+        removed_blocks.clear();
+    }
+
+    void mongo_db_deleter::delete_author_reward(bsoncxx::types::b_oid comment_oid) {
+        mongo_database["author_reward"].delete_one(document{} << "comment" << comment_oid << finalize);
+    }
+
+    void mongo_db_deleter::delete_benefactor_reward(bsoncxx::types::b_oid comment_oid) {
+        mongo_database["benefactor_reward"].delete_one(document{} << "comment" << comment_oid << finalize);
+    }
+
+    void mongo_db_deleter::delete_comment_content(bsoncxx::types::b_oid comment_oid) {
+        mongo_database["comment_content_object"].delete_one(document{} << "comment" << comment_oid << finalize);
+    }
+
+    void mongo_db_deleter::delete_comment_reward(bsoncxx::types::b_oid comment_oid) {
+        mongo_database["comment_reward"].delete_one(document{} << "comment" << comment_oid << finalize);
+    }
+
+    void mongo_db_deleter::delete_comment_vote(bsoncxx::types::b_oid comment_oid) {
+        mongo_database["comment_vote_object"].delete_many(document{} << "comment" << comment_oid << finalize);
+    }
+
+    void mongo_db_deleter::delete_curation_reward(bsoncxx::types::b_oid comment_oid) {
+        mongo_database["curation_reward"].delete_one(document{} << "comment" << comment_oid << finalize);
+    }
+
+    void mongo_db_deleter::delete_comment(const std::string& auth, const std::string& permlink) {
+        mongo_database["comment_object"].delete_one(document{} << "author" << auth << "permlink" << permlink << finalize);
+    }
+
+}}}
