1a971dee7891ce25fb4c54e5b0e3beac024faf70
Maslenitsa93 <maslenitsa93@gmail.com>
2018-05-27 11:36:06 +0300

    multi_index iterator replaced to find, oid improved, but still with shared_ptr

--- plugins/mongo_db/mongo_db_types.cpp
+++ plugins/mongo_db/mongo_db_types.cpp
@@ -5,34 +5,23 @@ namespace plugins {
 namespace mongo_db {
 
     void bmi_insert_or_replace(db_map& bmi, named_document doc) {
-        for (auto it = bmi.begin(); it != bmi.end(); ++it)
-        {
-            if (it->collection_name == doc.collection_name
-                && it->key == doc.key
-                && it->keyval == doc.keyval
-                && it->is_removal == doc.is_removal)
-            {
-                bmi.erase(it);
-                break;
-            }
-        }
+        auto it = bmi.get<hashed_idx>().find(std::make_tuple<std::string, std::string, std::string, bool>(
+            std::string(doc.collection_name),
+            std::string(doc.key), std::string(doc.keyval), bool(doc.is_removal)));
+        if (it != bmi.get<hashed_idx>().end())
+            bmi.get<hashed_idx>().erase(it);
         bmi.push_back(doc);
     }
 
     void bmi_merge(db_map& bmi_what, const db_map& bmi_with) {
         for (auto it = bmi_with.begin(); it != bmi_with.end(); ++it) {
+            // Erasing
+            auto it2 = bmi_what.get<hashed_idx>().find(std::make_tuple<std::string, std::string, std::string, bool>(
+                std::string(it->collection_name),
+                std::string(it->key), std::string(it->keyval), bool(it->is_removal)));
+            if (it2 != bmi_what.get<hashed_idx>().end())
+                bmi_what.get<hashed_idx>().erase(it2);
             // Adding
-            for (auto it2 = bmi_what.begin(); it2 != bmi_what.end(); ++it2)
-            {
-                if (it2->collection_name == it->collection_name
-                    && it2->key == it->key
-                    && it2->keyval == it->keyval
-                    && it2->is_removal == it->is_removal)
-                {
-                    bmi_what.erase(it2);
-                    break;
-                }
-            }
             bmi_what.push_back(*it);
         }
     }
