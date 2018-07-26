05f42ae22f91c2ce773bf2e49da88e191b36c574
Semen Medvedev <kiathai@gmail.com>
2018-07-26 18:44:51 +0700

    Refactor errors: private_message plugin #791

--- plugins/private_message/private_message_objects.cpp
+++ plugins/private_message/private_message_objects.cpp
@@ -1,10 +1,14 @@
+#include <golos/plugins/private_message/private_message_plugin.hpp>
 #include <golos/plugins/private_message/private_message_objects.hpp>
 #include <golos/protocol/operation_util_impl.hpp>
+
+#include <golos/protocol/exceptions.hpp>
 
 namespace golos { namespace plugins { namespace private_message {
 
     void private_message_operation::validate() const {
-        FC_ASSERT(from != to, "You cannot write to yourself");
+        GOLOS_CHECK_LOGIC(from != to, logic_errors::cannot_send_to_yourself,
+            "You cannot write to yourself");
     }
 } } } // golos::plugins::private_message
 
