9826989433e7207dbe1e7f3b6bd1d976fc450398
maslenitsa93 <maslenitsa93@gmail.com>
2019-01-23 20:37:20 +0300

    Worker proposal API functions #1026

--- plugins/worker_api/include/golos/plugins/worker_api/worker_api_queries.hpp
+++ plugins/worker_api/include/golos/plugins/worker_api/worker_api_queries.hpp
@@ -23,8 +23,27 @@ namespace golos { namespace plugins { namespace worker_api {
         }
     };
 
+    struct worker_techspec_query {
+        uint32_t                        limit = 20;
+        fc::optional<std::string>       start_author;
+        fc::optional<std::string>       start_permlink;
+        std::set<std::string>           select_authors;
+        fc::optional<std::string>       worker_proposal_author;
+        fc::optional<std::string>       worker_proposal_permlink;
+
+        void validate() const;
+
+        bool has_start() const {
+            return !!start_author;
+        }
+    };
+
 } } } // golos::plugins::worker_api
 
 FC_REFLECT((golos::plugins::worker_api::worker_proposal_query),
     (limit)(start_author)(start_permlink)(select_authors)(select_states)(select_types)
+);
+
+FC_REFLECT((golos::plugins::worker_api::worker_techspec_query),
+    (limit)(start_author)(start_permlink)(select_authors)(worker_proposal_author)(worker_proposal_permlink)
 );
