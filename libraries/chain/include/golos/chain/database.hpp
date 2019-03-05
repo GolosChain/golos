8ed6e8951dcd4d9e20165f3b147b43cdeed968fe
maslenitsa93 <maslenitsa93@gmail.com>
2019-03-05 19:10:02 +0300

    Clear old worker payment approves #1141

--- libraries/chain/include/golos/chain/database.hpp
+++ libraries/chain/include/golos/chain/database.hpp
@@ -123,6 +123,7 @@ namespace golos { namespace chain {
             bool store_memo_in_savings_withdraws() const;
 
             void set_clear_old_worker_techspec_approves(bool clear_old_worker_techspec_approves);
+            void set_clear_old_worker_payment_approves(bool clear_old_worker_payment_approves);
 
             /**
              * @brief wipe Delete database from disk, and potentially the raw chain as well.
@@ -276,6 +277,7 @@ namespace golos { namespace chain {
             void clear_expired_proposals();
 
             void clear_worker_techspec_approves(const worker_techspec_object& wto);
+            void clear_worker_payment_approves(const worker_techspec_object& wto);
 
             void clear_expired_worker_objects();
 
@@ -680,6 +682,7 @@ namespace golos { namespace chain {
             bool _store_memo_in_savings_withdraws = true;
 
             bool _clear_old_worker_techspec_approves = false;
+            bool _clear_old_worker_payment_approves = false;
 
             flat_map<std::string, std::shared_ptr<custom_operation_interpreter>> _custom_operation_interpreters;
             std::string _json_schema;
