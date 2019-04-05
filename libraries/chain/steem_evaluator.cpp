aa6e318c31c21a59d232afd15267458ac566ad5e
maslenitsa93 <maslenitsa93@gmail.com>
2019-04-05 19:49:17 +0300

    Remove min curation percent for witnesses #1009

--- libraries/chain/steem_evaluator.cpp
+++ libraries/chain/steem_evaluator.cpp
@@ -532,6 +532,12 @@ namespace golos { namespace chain {
                 }
 
                 GOLOS_CHECK_PARAM(percent, {
+                    if (!_db.has_hardfork(STEEMIT_HARDFORK_0_21__1009)) {
+                        GOLOS_CHECK_VALUE(STEEMIT_MIN_CURATION_PERCENT_PRE_HF21 <= percent && percent <= STEEMIT_MAX_CURATION_PERCENT,
+                            "Curation rewards percent must be between ${min} and ${max}.",
+                            ("min", STEEMIT_MIN_CURATION_PERCENT_PRE_HF21)("max", STEEMIT_MAX_CURATION_PERCENT));
+                    }
+
                     GOLOS_CHECK_VALUE(mprops.min_curation_percent <= ccrp.percent && ccrp.percent <= mprops.max_curation_percent,
                         "Curation rewards percent must be between ${min} and ${max}.",
                         ("min", mprops.min_curation_percent)("max", mprops.max_curation_percent));
@@ -858,7 +864,7 @@ namespace golos { namespace chain {
                         if (_db.has_hardfork(STEEMIT_HARDFORK_0_19__324)) {
                             com.curation_rewards_percent = mprops.min_curation_percent;
                         } else {
-                            com.curation_rewards_percent = STEEMIT_MIN_CURATION_PERCENT;
+                            com.curation_rewards_percent = STEEMIT_DEF_CURATION_PERCENT;
                         }
 
                         com.author = o.author;
